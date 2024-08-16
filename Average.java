a7be9998bbbcac0f4aa192afbdf4176f706e12ea
Akhilesh Soni <49095481+akhilswin@users.noreply.github.com>
2024-08-16 17:23:30 +0530

    Add files via upload
    This is the average calculation file to calculate average in TMS project

---
+++ Average.java
@@ -0,0 +1,16 @@
+
+public class Average {
+
+	public static void main(String[] args) {
+
+		int num1 = 8;
+		int num2 = 6;
+		int result;
+		
+		result = (num1 + num2) / 2;
+		
+		System.out.println("Average : "+result);
+
+	}
+
+}
